413a145cd5cdcf94b8a337a88ce7c16fed802714
Chris Andrews <candrews@sapient.com>
2016-02-11 10:52:40 +0000

    Adding config functionality, adding logging and security middleware

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,15 @@
+import config from 'config';
 import express from 'express';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
+
+if (config.get('features.logging')) {
+  app.use(morgan('combined'));
+}
+
+app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello');
