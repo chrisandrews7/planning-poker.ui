c7797e040d1b73deefb4cd00255c9ed290b0e205
Chris <chris@straza.co.uk>
2016-02-26 21:48:28 +0000

    Removing morgan

--- src/server/app.js
+++ src/server/app.js
@@ -1,26 +1,19 @@
-import config from 'config';
 import express from 'express';
 import helmet from 'helmet';
-import morgan from 'morgan';
 import bodyParser from 'body-parser';
 
 import indexRoutes from './controllers/index';
 
 const app = express();
 
-// Logging
-if (config.get('features.logging')) {
-  app.use(morgan('combined'));
-}
-
 // Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//   extended: false
+// }));
 
 // Security
-app.use(helmet());
+// app.use(helmet());
 
 // Views
 app.use(express.static(`${__dirname}/../client`));
