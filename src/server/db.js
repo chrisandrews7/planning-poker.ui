4c374ea806c52ba97328af3f0cb7dce9a0fe14d4
Chris Andrews <candrews@sapient.com>
2016-03-18 16:23:45 +0000

    Amending constant wording in db

--- src/server/db.js
+++ src/server/db.js
@@ -2,9 +2,8 @@ import config from 'config';
 import redis from 'redis';
 import fakeredis from 'fakeredis';
 
-const redisToUse = config.get('db.fake') ? fakeredis : redis;
-
-const client = redisToUse.createClient(config.get('db.port'), config.get('db.host'));
+const db = config.get('db.fake') ? fakeredis : redis;
+const client = db.createClient(config.get('db.port'), config.get('db.host'));
 
 if (config.get('db.auth')) {
     client.auth(config.get('db.password'));
