f601c001fd5d0bccfeb421a29c22b4d6a4306f0d
Chris <chris@straza.co.uk>
2016-10-26 20:30:02 +0100

    Removing index controller

--- src/server/app.js
+++ src/server/app.js
@@ -1,24 +1,12 @@
 import express from 'express';
 import helmet from 'helmet';
-import bodyParser from 'body-parser';
-
-import indexRoutes from './controllers/index';
 
 const app = express();
-
-// Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
 
 // Security
 app.use(helmet());
 
 // Views
-app.use(express.static(`${__dirname}/../client`));
-
-// Routes
-app.use(indexRoutes);
+app.use('/', express.static(`${__dirname}/../client`));
 
 export default app;
--- src/server/controllers/index.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('index');
-});
-
-export default router;
