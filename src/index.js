88597a4af41e440ef9437325eba39e149d61e1d7
Chris <chris@straza.co.uk>
2016-10-26 21:35:22 +0100

    Improving coverage

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 require('babel-core/register');
 require('babel-polyfill');
-require('./server').listen(require('config').get('port'), () => {
-	console.log('Server is running'); // eslint-disable-line
-});
+require('./server').listen(require('config').get('port'));
--- src/server/db.js
+++ src/server/db.js
@@ -5,8 +5,6 @@ import fakeredis from 'fakeredis';
 const db = config.get('db.fake') ? fakeredis : redis;
 const client = db.createClient(config.get('db.port'), config.get('db.host'));
 
-if (config.get('db.auth')) {
-  client.auth(config.get('db.password'));
-}
+client.auth(config.get('db.password'));
 
 export default client;
