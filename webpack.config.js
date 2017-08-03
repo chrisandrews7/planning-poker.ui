e4ede6743e9a6674c78a9eed9f1e873faaffa19c
Chris <chris@straza.co.uk>
2017-08-03 17:22:06 +0100

    Updating webpack config to V3 guidelines

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,50 @@
 var webpack = require('webpack');
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
-    'webpack-dev-server/client?http://localhost:8080',
-    'webpack/hot/only-dev-server',
-    './src/client/index.js'
+    'babel-polyfill',
+    './src/index.js'
   ],
   module: {
-    loaders: [{
-      test: /\.js|jsx$/,
-      exclude: /node_modules/,
-      loader: 'react-hot!babel'
-    }]
+    rules: [
+      {
+        test: /\.js|jsx$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader'
+          ]
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: {
+          loader: 'file-loader',
+          options: 'images/[name].[ext]'
+        }
+      }
+    ]
   },
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['.js', '.jsx']
   },
   output: {
     path: 'dist',
     publicPath: '/',
     filename: 'bundle.js'
   },
-  devServer: {
-    contentBase: 'dist',
-    hot: true,
-    inline: true,
-    historyApiFallback: true
-  },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    new ExtractTextPlugin('styles.css'),
     new HtmlWebpackPlugin({
       title: 'Planning Poker'
     })
