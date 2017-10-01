450d7f1c0cbb78c60e164f475318b8bc861bf923
Chris <chris@straza.co.uk>
2017-10-01 22:10:08 +0100

    Removing redirect middleware

--- src/middleware/redirect.js
+++
@@ -1,8 +0,0 @@
-import { browserHistory } from 'react-router';
-
-export default () => next => (action) => {
-  if (action.meta && action.meta.redirect) {
-    browserHistory.push(`${action.meta.redirect}`);
-  }
-  return next(action);
-};
