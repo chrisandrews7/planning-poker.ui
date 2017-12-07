abcaf003fdd1658fc7391d56a974d2f807f8e68d
Chris <chris@straza.co.uk>
2017-12-07 21:33:23 +0000

    Added player selectors

---
+++ src/selectors/players.spec.js
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import { fromJS } from 'immutable';
+import faker from 'faker';
+import { selectAllPlayers } from './players';
+
+describe('Players Selectors', () => {
+  describe('selectAllPlayers()', () => {
+    it('should return a full list of all players including the user', () => {
+      expect(selectAllPlayers(fromJS({
+        players: {
+          Dave: {
+            name: 'Dave',
+            vote: 10
+          }
+        },
+        user: {
+          name: 'Steve',
+          vote: 5
+        }
+      }))).to.deep.equal({
+        Dave: {
+          name: 'Dave',
+          vote: 10
+        },
+        Steve: {
+          name: 'Steve',
+          vote: 5
+        }
+      });
+    });
+  });
+});
