bcb1cfe93e238ed815be25ec545ce1dcd14a3f0a
Chris <chris@straza.co.uk>
2018-09-09 20:34:02 +0100

    Move root into index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { render } from 'react-dom';
 import io from 'socket.io-client';
 import config from 'enviro-conf';
+import { Provider } from 'react-redux';
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom';
+
 import configureStore from './store/configureStore';
-import Root from './containers/Root';
+import GameContainer from './containers/Game';
 
 const SERVER_URL = config.get('SERVER_URL');
 
@@ -11,6 +17,10 @@ const socket = io(SERVER_URL);
 const store = configureStore(undefined, socket);
 
 render(
-  <Root store={store} />,
+  <Provider store={store}>
+    <Router>
+      <Route path="/:gameId" component={GameContainer} />
+    </Router>
+  </Provider>,
   document.body.appendChild(document.createElement('div'))
 );
