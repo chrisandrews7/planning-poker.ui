cc4b79e3d97297c4d31665d8022c8d8e922b310f
Chris <chris@straza.co.uk>
2018-09-09 20:34:26 +0100

    Add socket middleware to store config

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/index';
 
-import socketMiddleware from '../middleware/socket/';
-import getStateMiddleware from '../middleware/getState';
+import listenersMiddleware from '../middleware/socket';
+import emittersMiddleware from '../middleware/socket/emitters';
 
 export default function (initialState, socket) {
   // @todo: Use new dev tool extention method
   const createStoreWithMiddleware = applyMiddleware(
-    getStateMiddleware,
-    socketMiddleware(socket)
+    emittersMiddleware(socket),
+    listenersMiddleware(socket)
   )(createStore);
 
   const store = createStoreWithMiddleware(
