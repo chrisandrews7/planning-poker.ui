0264ab0717819b56ae4fd26c7859e8da576f6356
Chris <chris@straza.co.uk>
2019-01-05 23:38:39 +0000

    Results

---
+++ src/components/Results/index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { orderBy } from 'lodash';
+import './styles.less';
+
+const Results = ({ results }) => {
+  const bars = results.reduce((group, result) => ({
+    ...group,
+    [result]: {
+      value: result,
+      occurence: group[result] ? group[result].occurence + 1 : 1
+    }
+  }), {});
+
+  const orderedBars = orderBy(bars, ['occurence'], ['desc']);
+  const maxBar = orderedBars[0].occurence;
+
+  return (
+    <div className="results">
+      {orderedBars.map(({ occurence, value }) => (
+        <div className="progress" key={value}>
+          <div
+            className="progress-bar bg-info"
+            style={{
+              width: `${(occurence / maxBar) * 100}%`
+            }}
+          >
+            {value}
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+Results.propTypes = {
+  results: PropTypes.arrayOf(PropTypes.string).isRequired
+};
+
+export default Results;
---
+++ src/components/Results/index.spec.js
@@ -0,0 +1,75 @@
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import React from 'react';
+import Results from '.';
+
+describe('Results', () => {
+  it('renders a bar for each unique result', () => {
+    const wrapper = shallow(<Results results={['test1', 'test1', 'test2']} />);
+
+    expect(wrapper.find('.progress-bar')).to.have.a.lengthOf(2);
+
+    expect(wrapper
+      .containsMatchingElement(
+        <div>test1</div>
+      )).to.be.ok;
+
+    expect(wrapper
+      .containsMatchingElement(
+        <div>test2</div>
+      )).to.be.ok;
+  });
+
+  it('sets the width of each bar based on a percentage of the total occurences of each unique result', () => {
+    const wrapper = shallow(<Results results={[
+      'test1',
+      'test1',
+      'test1',
+      'test1', // 4 occurences = 100%
+      'test2',
+      'test2', // 2 ocurrences = 50%
+      'test3' // 1 occurence = 25%
+    ]}
+    />);
+
+    expect(wrapper
+      .containsMatchingElement(
+        <div style={{ width: '100%' }}>test1</div>
+      )).to.be.ok;
+
+    expect(wrapper
+      .containsMatchingElement(
+        <div style={{ width: '50%' }}>test2</div>
+      )).to.be.ok;
+
+    expect(wrapper
+      .containsMatchingElement(
+        <div style={{ width: '25%' }}>test3</div>
+      )).to.be.ok;
+  });
+
+  it('renders the bar with the most votes the full width of the component', () => {
+    const wrapper = shallow(<Results results={['test1', 'test1', 'test2']} />);
+
+    expect(wrapper
+      .containsMatchingElement(
+        <div style={{ width: '100%' }}>test1</div>
+      )).to.be.ok;
+  });
+
+  it('sorts the bars based on the results total occurences', () => {
+    const wrapper = shallow(<Results results={[
+      'test1',
+      'test1',
+      'test1',
+      'test2',
+      'test2',
+      'test3'
+    ]}
+    />);
+
+    expect(wrapper.find('.progress-bar').at(0).text()).to.equal('test1');
+    expect(wrapper.find('.progress-bar').at(1).text()).to.equal('test2');
+    expect(wrapper.find('.progress-bar').at(2).text()).to.equal('test3');
+  });
+});
