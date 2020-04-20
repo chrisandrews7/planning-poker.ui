dee134c32e74035e34d045d8e374cf1c9f3e4811
chrisandrews7 <chrisandrews7@users.noreply.github.com>
2020-04-20 16:43:55 +0100

    Remove total votes

--- src/components/Results/index.js
+++ src/components/Results/index.js
@@ -25,7 +25,7 @@ const Results = ({ results }) => {
               width: `${(occurence / maxBar) * 100}%`
             }}
           >
-            {`${value} (${occurence} votes)`}
+            {value}
           </div>
         </div>
       ))}
